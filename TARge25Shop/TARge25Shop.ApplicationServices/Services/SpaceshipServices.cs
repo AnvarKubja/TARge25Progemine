44d28c4fbacfbd0bd68123fd24208205907e6eba
Ossu-V <aadukubja@gmail.com>
2026-09-07 16:17:10 +0300

    Spaceship Domain

---
+++ TARge25Shop/TARge25Shop.ApplicationServices/Services/SpaceshipServices.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using TARge25Shop.Core.Domain;
+using TARge25Shop.Core.Dto;
+
+namespace TARge25Shop.ApplicationServices.Services
+{
+    public class SpaceshipServices
+    {
+        public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+
+            return null;
+        }
+    }
+}
