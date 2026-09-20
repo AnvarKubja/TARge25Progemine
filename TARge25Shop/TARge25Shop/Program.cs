8f8f1bcfbf479fb9abee4ee9645acd0e49023964
AnvarKubja <aadukubja@gmail.com>
2026-09-20 15:32:27 +0300

    Dependency injection, Index, Update, Create, Detail and Delete viewmodels

--- TARge25Shop/TARge25Shop/Program.cs
+++ TARge25Shop/TARge25Shop/Program.cs
@@ -18,6 +18,7 @@ namespace TARge25Shop
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+            builder.Services.AddScoped<IKindergartenServices, KindergartenServices>();
 
 
             var app = builder.Build();
