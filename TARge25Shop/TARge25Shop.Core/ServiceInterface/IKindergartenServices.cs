250650177a48d18b87dde2b990a9d24fda6a8d0c
AnvarKubja <aadukubja@gmail.com>
2026-09-20 15:06:28 +0300

    Dto + Interface

---
+++ TARge25Shop/TARge25Shop.Core/ServiceInterface/IKindergartenServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TARge25Shop.Core.ServiceInterface
+{
+    public interface IKindergartenServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> Update(KindergartenDto dto);
+        Task<Kindergarten> DetailAsync(KindergartenDto dto);
+        Task<Kindergarten> Delete(KindergartenDto dto);
+    }
+}
